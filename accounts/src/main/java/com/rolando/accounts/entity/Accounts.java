f6e864f8f7b9ac343add07cc518c653d9bd1dc68
pinchiri <rolando.sorrentino@correo.unimet.edu.ve>
2025-01-12 20:41:11 -0400

    feat: ✨ Writing Spring Data JPA entities & repositories to interact with DB tables

---
+++ accounts/src/main/java/com/rolando/accounts/entity/Accounts.java
@@ -0,0 +1,14 @@
+package com.rolando.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity {
+    @Id
+    private Long accountNumber;
+    private Long customerId;
+    private String accountType;
+    private String branchAddress;
+}
