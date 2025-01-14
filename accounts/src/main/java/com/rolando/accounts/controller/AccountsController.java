079859196a3ffe018de82e9824e1854620b5db46
Rolando Sorrentino <rolando4693@gmail.com>
2025-01-14 13:38:41 -0400

    feat: ✨ Create account endpoint finished, GlobalExceptionHandler added

--- accounts/src/main/java/com/rolando/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/rolando/accounts/controller/AccountsController.java
@@ -3,6 +3,9 @@ package com.rolando.accounts.controller;
 import com.rolando.accounts.constants.AccountsConstants;
 import com.rolando.accounts.dto.CustomerDto;
 import com.rolando.accounts.dto.ResponseDto;
+import com.rolando.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +14,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
-    @PostMapping
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+
+    private IAccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+
+        ResponseDto responseDto = new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseDto);
     }
 }
 
--- accounts/src/main/java/com/rolando/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/rolando/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.rolando.accounts.service.impl;
 
+import com.rolando.accounts.constants.AccountsConstants;
 import com.rolando.accounts.dto.CustomerDto;
+import com.rolando.accounts.entity.Accounts;
+import com.rolando.accounts.entity.Customer;
+import com.rolando.accounts.exception.CustomerAlreadyExistsException;
+import com.rolando.accounts.mapper.CustomerMapper;
 import com.rolando.accounts.repository.AccountsRepository;
 import com.rolando.accounts.repository.CustomerRepository;
 import com.rolando.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -21,6 +30,33 @@ public class AccountsServiceImpl implements IAccountsService {
      */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
 
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobile number: " + customerDto.getMobileNumber());
+        }
+
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = generateRandomAccountNumber();
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
+    }
+
+    private long generateRandomAccountNumber() {
+        return 1000000000L + new Random().nextInt(900000000);
     }
 }
